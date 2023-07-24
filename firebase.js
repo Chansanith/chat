67c2f83b0fb3ca61d5f9c54016484da4f93d9d59
Chansanith <33217719+Chansanith@users.noreply.github.com>
2023-07-25 01:45:09 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 firebase.initializeApp({
-  apiKey: "AIzaSyC0dcHIWZHM8bgqCy-VtHnhdi6xZ4AUCHQ",
-  authDomain: "chat-app-481d2.firebaseapp.com",
-  projectId: "chat-app-481d2",
-  storageBucket: "chat-app-481d2.appspot.com",
-  messagingSenderId: "282153294471",
-  appId: "1:282153294471:web:dd62013c1ddc99ceecb622",
-  measurementId: "G-E4RMPSGS00"
+    apiKey: "AIzaSyC0dcHIWZHM8bgqCy-VtHnhdi6xZ4AUCHQ",
+    authDomain: "chat-app-481d2.firebaseapp.com",
+    projectId: "chat-app-481d2",
+    storageBucket: "chat-app-481d2.appspot.com",
+    messagingSenderId: "282153294471",
+    appId: "1:282153294471:web:dd62013c1ddc99ceecb622",
+    measurementId: "G-E4RMPSGS00"
 });
 const db = firebase.firestore();
 
